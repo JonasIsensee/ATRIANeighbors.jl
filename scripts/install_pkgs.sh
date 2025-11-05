2201a0cbdfed2973df25fba38657e4cd50836508
Jonas Isensee <jonas.isensee@web.de>
2025-11-05 23:26:10 +0100

    make v1.11 the default

--- scripts/install_pkgs.sh
+++ scripts/install_pkgs.sh
@@ -8,6 +8,8 @@ fi
 curl -fsSL https://install.julialang.org > juliaup.sh
 sh juliaup.sh -y --default-channel release
 $HOME/.juliaup/bin/juliaup add release
+$HOME/.juliaup/bin/juliaup add 1.11
+$HOME/.juliaup/bin/juliaup default 1.11
 $HOME/.juliaup/bin/juliaup config channelsymlinks true
 
 if [ -n "$CLAUDE_ENV_FILE" ]; then
